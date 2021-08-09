c3e65152959ab332f197c7580c2d26c2432d7921
dengziheng <zihengdeng@outlook.com>
2021-08-09 23:50:44 +0800

    style:修改包名

--- src/main/java/dengzh/hot100/package-info.java
+++ src/main/java/dengzh/hot100/package-info.java
@@ -3,4 +3,4 @@
  * @Author dengziheng
  * @Date 2021/8/9 23:08
  */
-package dengzh.Hot100;
+package dengzh.hot100;
--- src/main/java/dengzh/offer/Offer13.java
+++ src/main/java/dengzh/offer/Offer13.java
@@ -1,4 +1,4 @@
-package dengzh.Offer;
+package dengzh.offer;
 
 /**
  * @Description 剑指 Offer 13. 机器人的运动范围
