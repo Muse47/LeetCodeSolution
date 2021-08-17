b0bb59a4d8788292f2ec5ff32c9f917dbed190b0
dengziheng <zihengdeng@outlook.com>
2021-08-18 00:43:39 +0800

    feat:offer32-2

---
+++ src/main/java/dengzh/offer/Offer32_2.java
@@ -0,0 +1,51 @@
+package dengzh.offer;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @Description 剑指 Offer 32 - II. 从上到下打印二叉树 II
+ * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+ * @Author dengziheng
+ * @Date 2021/8/18 00:19
+ */
+public class Offer32_2 {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>() {
+            {
+                add(root);
+            }
+        };
+        while (!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
